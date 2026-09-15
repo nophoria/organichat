f309dbbb16125dbbf7021065d4ffa830239aa014
nophoria <pigable768@gmail.com>
2026-09-15 20:41:38 +0100

    made log seperate data into list

--- backendtest.py
+++ backendtest.py
@@ -8,7 +8,22 @@ Server = backend.Server(port=6567, debug=True, logpath="test.log")
 Server.Log("Test", 4)
 Server.Log("Test", 4)
 
+LogList = []
 with open("test.log", "r") as LogFile:
-    LogContents = LogFile.read()
+    LogContents = LogFile.readlines()
 
-print(LogContents)
+    linenum = 0
+    for line in LogContents:
+        linenum += 1
+        linedata = line.split(", ")
+        LogList.append(
+            {
+                "line": linenum,
+                "time": linedata[0],
+                "loglevel": linedata[1],
+                "data": linedata[2:]
+            }
+        )
+
+
+print(LogList)
