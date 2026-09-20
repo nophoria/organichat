58600e34b569ea9702022a0145894bd39018b997
Theunderlord9 <jack2011strong@gmail.com>
2026-09-20 11:04:16 +0100

    Started Actually Doing The New Backend, Client Conn Is Handled

--- backend.py
+++ backend.py
@@ -7,58 +7,23 @@ def HashDat(Hash, Dat):
     return bytes(Byte ^ Hash[i % len(Hash)] for i, Byte in enumerate(Dat))
 
 class BackendHandler:
-    def __init__(self, ServerIP="localhost", Port=6567, Pass="InputAPasswordMoron"):
+    def __init__(self, ServerIP="localhost", Port=6567, Pass="InputAPasswordMoron", Username="User"):
         self.ServerIP = ServerIP
         self.Port = Port
         self.Pass = Pass
+        self.Username = Username
         self.Hash = (hashlib.sha512(Pass.encode('utf-32'))).digest()
 
-#     def SendMsg(self, Msg, MaxRetries=5):
-#         for Attempt in range(MaxRetries):
-#             try:
-#                 ClientSock = Sock.socket(Sock.AF_INET, Sock.SOCK_STREAM)
-#                 ClientSock.connect((self.ServerIP, self.Port))
-#                 EncodedMsg = Msg.encode("utf-32")
-#                 if len(EncodedMsg) > 8188:
-#                     return False
-#                 Header = bytearray(1)
-#                 Header[0] = 0
-#                 Payload = Header + EncodedMsg
-#                 ClientSock.send(HashDat(self.Hash, Payload))
-#                 ClientSock.close()
-#                 return True
-#             except Exception as e:
-#                 if hasattr(ClientSock, 'close'):
-#                     ClientSock.close()
-#                 if Attempt < MaxRetries - 1:
-#                     time.sleep(1)
-#                 continue
-#         return False
+    def Conn(self):
+        try:
+            # Create Socket
+            ClientSocket = Sock.socket(Sock.AF_INET, Sock.SOCK_STREAM)
+            ClientSocket.connect((self.ServerIP, self.Port))
+            ClientSocket.send(HashDat(self.Hash, Username))
 
-#     def PullMsgs(self):
-#         try:
-#             ClientSock = Sock.socket(Sock.AF_INET, Sock.SOCK_STREAM)
-#             ClientSock.connect((self.ServerIP, self.Port))
-#             Header = bytearray(1)
-#             Header[0] = 128
-#             ClientSock.send(Header)
-
-#             Response = b""
-#             while True:
-#                 chunk = ClientSock.recv(8192)
-#                 if not chunk:
-#                     break
-#                 Response += chunk
-                
-#             ClientSock.close()
-
-#             MsgList = HashDat(self.Hash, (Response))
-#             MsgList = MsgList.decode("utf-32")
-#             MsgList = MsgList.split(",[NewMsg] ")
-#             return MsgList
-
-#         except Exception as e:
-#             return False
+        except Exception as e:
+            print(f"Error Connecting To Server: {e}")
+            return None
 
 class Server:
     def __init__(self, port=6567, debug=False, logpath="server.log", Pass="InputAPasswordMoron"):
@@ -73,14 +38,15 @@ class Server:
         # Init Globals
         self.UserList = []
 
-    def HandleClient(ClientSock, ClientAddrSanitised):
+    def HandleClient(self, ClientSock, ClientAddrSanitised):
         Connected = True
         while Connected:
             try:
                 Dat = ClientSock.recv(1024)
-                DecodedDat = HashDat(self.Hash, Dat)
+                DecodedDat = str(HashDat(self.Hash, Dat))
                 self.UserList.append((ClientAddrSanitised, DecodedDat))
-                
+                ReturnUserList = str(self.UserList).encode("utf-32")
+                ClientSock.send(HashDat(self.Hash, ReturnUserList))
 
             except Exception as e:
                 self.Log(f"TCP Conn Err {ClientAddrSanitised}: {e}", ErrLevel=2)
@@ -102,44 +68,12 @@ class Server:
             self.Log(f"Client Connected From {ClientAddrSanitised}", ErrLevel=0)
             
             # Create New Thread Per Conn
-            client_thread = threading.Thread(
+            ClientThread = threading.Thread(
                 target=self.HandleClient,
                 args=(ClientSock, ClientAddrSanitised)
             )
-            client_thread.daemon = True
-            client_thread.start()
-#             try:
-#                 Dat = ClientSock.recv(8193)
-#                 DecodedDat = HashDat(self.Hash, Dat)
-#                 HeaderDat = bytes([Dat[0]])
-#                 HeaderDat = HashDat(self.Hash, HeaderDat)
-
-#                 if len(HeaderDat) == 0:
-#                     ClientSock.close()
-#                     continue
-#                 HeaderByte = HeaderDat[0]
-#                 IsPull = bool(HeaderByte & 128)
-
-#                 if IsPull:
-#                     self.Log("Client Requested Msg History", ErrLevel=0)
-#                     ClientSock.send(HashDat(self.Hash, ((",[NewMsg] ".join(self.MsgHistory)).encode("utf-32"))))
-                
-#                 else:
-#                     self.Log("Client Sent New Msg", ErrLevel=0)
-#                     Message = DecodedDat[1:8193].decode("utf-32")
-#                     if len(Message) > 0:
-#                         if self.debug:
-#                             self.Log(f"Message Received From {ClientAddrSanitised} [{Message}]", ErrLevel=0)
-#                         else:
-#                             self.Log(f"Message Received From {ClientAddrSanitised}", ErrLevel=0)
-
-#                         self.MsgHistory.append(Message)
-#                         self.Log(self.MsgHistory, ErrLevel=4)
-
-#             except Exception as e:
-#                 self.Log(f"TCP Conn Err {ClientAddrSanitised}: {e}", ErrLevel=2)
-#             finally:
-#                 ClientSock.close()
+            ClientThread.daemon = True
+            ClientThread.start()
 
     def Stop(self):
         self.running = False
